a82480fd74116a0546fe8e4930515e11e96a3acd
kyoung90 <kenneth.young90@gmail.com>
2019-09-18 10:07:18 -0400

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-    user1 = User.create!(name: "Kenneth", email: "kenneth.young90@gmail.com", gender: "Male", age: 23, bio: "This is a bio", password: "topsecret", password_confirmation: "topsecret")
-    user2 = User.create!(name: "Gabriela", email: "gabs123@gmail.com", gender: "Female", age: 20, bio: "This is a bio", password: "topsecret", password_confirmation: "topsecret")
-    user3 = User.create!(name: "Roberto", email: "robs123@gmail.com", gender: "Male", age: 24, bio: "This is a bio", password: "topsecret", password_confirmation: "topsecret")
+user1 = User.create!(name: "Kenneth", email: "kenneth.young90@gmail.com", gender: "Male", age: 23, bio: "This is a bio", password: "topsecret", password_confirmation: "topsecret")
+user2 = User.create!(name: "Gabriela", email: "gabs123@gmail.com", gender: "Female", age: 20, bio: "This is a bio", password: "topsecret", password_confirmation: "topsecret")
+user3 = User.create!(name: "Roberto", email: "robs123@gmail.com", gender: "Male", age: 24, bio: "This is a bio", password: "topsecret", password_confirmation: "topsecret")
 
-    liked_users1 = LikedUser.create(original_user_id: 1, liked_user_id: 2)
-    liked_users2 = LikedUser.create(original_user_id: 1, liked_user_id: 3)
-    liked_users3 = LikedUser.create(original_user_id: 2, liked_user_id: 1)
-    liked_users4 = LikedUser.create(original_user_id: 3, liked_user_id: 1)
+liked_users1 = LikedUser.create(original_user_id: 1, liked_user_id: 2)
+liked_users2 = LikedUser.create(original_user_id: 1, liked_user_id: 3)
+liked_users3 = LikedUser.create(original_user_id: 2, liked_user_id: 1)
+liked_users4 = LikedUser.create(original_user_id: 3, liked_user_id: 1)
 
+skill1 = Skill.create(name: "Javascript")
+skill2 = Skill.create(name: "Forex Trading")
+
+resource1 = Resource.create(name: "Udemy")
+resource2 = Resource.create(name: "Babypips")
+
+resource_skill1 = ResourceSkill.create(skill_id: 1, resource_id: 1)
+resource_skill2 = ResourceSkill.create(skill_id: 2, resource_id: 2)
+
+user_skill1 = UserSkill.create(user_id: 1, skill_id: 1, mastery: "intermediate")
+user_skill2 = UserSkill.create(user_id: 1, skill_id: 2, mastery: "beginner")
+
+user_skill3 = UserSkill.create(user_id: 2, skill_id: 1, mastery: "beginner")
+user_skill4 = UserSkill.create(user_id: 2, skill_id: 2, mastery: "beginner")
+
+user_skill5 = UserSkill.create(user_id: 3, skill_id: 1, mastery: "beginner")
+user_skill6 = UserSkill.create(user_id: 3, skill_id: 2, mastery: "beginner")
