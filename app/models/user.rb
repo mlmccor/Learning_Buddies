434a4ca0b14fcd73d62c99e620768488926ad556
kyoung90 <kenneth.young90@gmail.com>
2019-09-18 08:50:12 -0400

    Users serializer now shows all liked users data

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 
-  has_many :original_users, class_name: 'LikedUser', foreign_key: :original_user_id
-  has_many :liked_users, class_name: 'LikedUser', foreign_key: :liked_user_id
+  has_many :original_users, class_name: 'LikedUser', foreign_key: :liked_user_id
+  has_many :liked_users, class_name: 'LikedUser', foreign_key: :original_user_id
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :liked_users
+  attributes :id, :email, :name, :age, :gender, :bio, :liked_users
 
   def liked_users 
-    return object.liked_users
+    
+    users = object.liked_users.collect do |liked_user|
+      user = User.find_by(id: liked_user.liked_user_id)
+    end
 
   end 
 end
