45c84f52e482f1521916b8408cbbabfb2e4abe0a
kyoung90 <kenneth.young90@gmail.com>
2019-09-18 09:15:44 -0400

    Changed what data is being serialized in liked_users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,7 +4,9 @@ class UserSerializer < ActiveModel::Serializer
   def liked_users 
     
     users = object.liked_users.collect do |liked_user|
-      user = User.find_by(id: liked_user.liked_user_id)
+      # user = User.find_by(id: liked_user.liked_user_id)
+      # user.attributes.except!("created_at", :updated_at, :email)
+      user = User.select(:id, :name, :gender, :age, :bio).find_by(id: liked_user.liked_user_id)
     end
 
   end 
