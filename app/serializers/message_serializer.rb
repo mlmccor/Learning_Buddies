59111e48dc001a22822da08cdb6e64313ff0ae04
kyoung90 <kenneth.young90@gmail.com>
2019-09-18 09:22:50 -0400

    Added routes and serializers

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,3 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :sender_id, :receiver_id, :conversation_id, :message
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
   resources :users, only: [:show, :index]
+  resources :resources, only: [:show, :index]
+  resources :messages, only: [:show, :index]
+  resources :skills, only: [:show, :index]
 
   devise_for :users,
   path: '',
